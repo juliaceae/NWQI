c4be0f7af2400e10622414a149adcca6d33de738
juliaceae <crown.julia@deq.state.or.us>
2016-07-25 16:18:25 -0700

    Re-wrote interpretations for clarity (and correctedness). Tested LRBS values excluding RBS values with Google earth slope values (BTW, same conclusion - no significant increase or decrease).

--- NWQI_15M_2015_TTests.R
+++ NWQI_15M_2015_TTests.R
@@ -25,6 +25,9 @@ plot(RBS$Post.LRBS-RBS$Pre.LRBS,
      )
 abline(0,0, col="blue", lwd=2)
 
+##################################################################################
+#Are any trends getting better after BMP implementation?
+
 #2005/06 and 2015 RBS LRBS values
 t.test(RBS$Pre.LRBS, RBS$Post.LRBS, paired=TRUE, alternative="less")
 # Paired t-test
@@ -37,160 +40,25 @@ t.test(RBS$Pre.LRBS, RBS$Post.LRBS, paired=TRUE, alternative="less")
 # sample estimates:
 #   mean of the differences 
 # 0.1578175 
-# Conclusion: Post-implementation LRBS values are greater than or equal to Pre-implementation LRBS values.
-
-#2005/06 and 2015 RBS percent sand and fines values
-t.test(RBS$Pre.Percent.Sand.and.Fines..PCT_SAFN., as.numeric(RBS$Post.Percent.Sand.and.Fines..PCT_SAFN.), paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  RBS$Pre.Percent.Sand.and.Fines..PCT_SAFN. and as.numeric(RBS$Post.Percent.Sand.and.Fines..PCT_SAFN.)
-# t = -0.17097, df = 11, p-value = 0.4337
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 4.558904
-# sample estimates:
-#   mean of the differences 
-# -0.4796667 
-
-#2005/06 and 2015 RBS percent sand values
-t.test(RBS$Pre.Percent.Sand..PCT_SA., RBS$Post.Percent.Sand..PCT_SA., paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  RBS$Pre.Percent.Sand..PCT_SA. and RBS$Post.Percent.Sand..PCT_SA.
-# t = -0.78705, df = 4, p-value = 0.2376
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 5.724318
-# sample estimates:
-#   mean of the differences 
-# -3.3502 
-
-#2005/06 and 2015 RBS percent fines values
-t.test(RBS$Pre.Percent.Fines..PCT_FN., RBS$Post.Percent.Fines..PCT_FN., paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  RBS$Pre.Percent.Fines..PCT_FN. and RBS$Post.Percent.Fines..PCT_FN.
-# t = 1.0441, df = 4, p-value = 0.8223
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 16.71406
-# sample estimates:
-#   mean of the differences 
-# 5.4948 
-
-#1994 and 2015 Wolman D50 values
-t.test(Wolman$Pre.1994.D50..mm, Wolman$Post.2015.D50..mm, paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  Wolman$Pre.1994.D50..mm and Wolman$Post.2015.D50..mm
-# t = -2.5404, df = 3, p-value = 0.04232
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf -0.2730085
-# sample estimates:
-#   mean of the differences 
-# -3.7075 
-# Conclusion: At 4 sites, D50 from Wolman pebble counts in 2015 D50 were less than D50 in 1994.
-
-#1994 and 2015 Wolman percent less than 1 mm values
-t.test(Wolman$Pre.1994.percent.less.1mm, Wolman$Post.2015.Percent.less.1mm, paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  Wolman$Pre.1994.percent.less.1mm and Wolman$Post.2015.Percent.less.1mm
-# t = 3.5614, df = 3, p-value = 0.9811
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 22.26291
-# sample estimates:
-#   mean of the differences 
-# 13.405 
-
-#1994 and 2015 Wolman percent sand and fines values
-t.test(Wolman$Pre.1994.percent.SAFN, Wolman$Post.2015.Percent.SAFN, paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  Wolman$Pre.1994.percent.SAFN and Wolman$Post.2015.Percent.SAFN
-# t = 2.7951, df = 3, p-value = 0.9659
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 16.86315
-# sample estimates:
-#   mean of the differences 
-# 9.155 
-
-#1994 and 2015 Wolman percent gravel values
-t.test(Wolman$Pre.1994.percent.gravel...fines, Wolman$Post.2015Percent.gravel...fines, paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  Wolman$Pre.1994.percent.gravel...fines and Wolman$Post.2015Percent.gravel...fines
-# t = 2.4381, df = 3, p-value = 0.9537
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 12.09106
-# sample estimates:
-#   mean of the differences 
-# 6.1525 
-
-#2000 and 2015 Wolman D50 values
-t.test(Wolman$Pre.2000.D50..mm, Wolman$Post.2015.D50..mm, paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  Wolman$Pre.2000.D50..mm and Wolman$Post.2015.D50..mm
-# t = -0.53835, df = 10, p-value = 0.3011
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 5.383153
-# sample estimates:
-#   mean of the differences 
-# -2.274545 
-
-#2000 and 2015 Wolman percent less than 1 mm values
-t.test(Wolman$Pre.2000.Percent.Less.1mm, Wolman$Post.2015.Percent.less.1mm, paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  Wolman$Pre.2000.Percent.Less.1mm and Wolman$Post.2015.Percent.less.1mm
-# t = 0.088766, df = 10, p-value = 0.5345
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 7.59378
-# sample estimates:
-#   mean of the differences 
-# 0.3545455 
-
-#2000 and 2015 Wolman percent sand and fines values
-t.test(Wolman$Pre.2000.PercentSAFN, Wolman$Post.2015.Percent.SAFN, paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  Wolman$Pre.2000.PercentSAFN and Wolman$Post.2015.Percent.SAFN
-# t = -0.80785, df = 10, p-value = 0.219
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 3.38588
-# sample estimates:
-#   mean of the differences 
-# -2.722727 
-
-#2000 and 2015 Wolman percent gravel values
-t.test(Wolman$Pre.2000.Percent.Gravel...fines, Wolman$Post.2015Percent.gravel...fines, paired=TRUE, alternative="less")
-# Paired t-test
-# 
-# data:  Wolman$Pre.2000.Percent.Gravel...fines and Wolman$Post.2015Percent.gravel...fines
-# t = -0.56704, df = 10, p-value = 0.2916
-# alternative hypothesis: true difference in means is less than 0
-# 95 percent confidence interval:
-#   -Inf 4.594405
-# sample estimates:
-#   mean of the differences 
-# -2.091818 
-
-##################################################################################
-#Are any trends getting worse?
-
-#2005/06 and 2015 RBS LRBS values
-t.test(RBS$Pre.LRBS, RBS$Post.LRBS, paired=TRUE, alternative="greater")
+# Conclusion: Post-implementation LRBS values could be less than or equal to Pre-implementation LRBS values.
+# This is not as expected.
+# Values are not getting significantly larger, as we had hoped to see post implementation.
 
 #2005/06 and 2015 RBS percent sand and fines values
 t.test(RBS$Pre.Percent.Sand.and.Fines..PCT_SAFN., as.numeric(RBS$Post.Percent.Sand.and.Fines..PCT_SAFN.), paired=TRUE, alternative="greater")
+# Paired t-test
+# 
+# data:  RBS$Pre.Percent.Sand.and.Fines..PCT_SAFN. and as.numeric(RBS$Post.Percent.Sand.and.Fines..PCT_SAFN.)
+# t = -0.17097, df = 11, p-value = 0.5663
+# alternative hypothesis: true difference in means is greater than 0
+# 95 percent confidence interval:
+#   -5.518238       Inf
+# sample estimates:
+#   mean of the differences 
+# -0.4796667
+# Conclusion: Post-implementation percent sand and fines values could be greater than or equal to Pre-implementation percent sand and fines values.
+# This is not as expected.
+# Values are not getting significantly smaller, as we had hoped to see post implementation.
 
 #2005/06 and 2015 RBS percent sand values
 t.test(RBS$Pre.Percent.Sand..PCT_SA., RBS$Post.Percent.Sand..PCT_SA., paired=TRUE, alternative="greater")
@@ -214,7 +82,7 @@ t.test(Wolman$Pre.1994.percent.less.1mm, Wolman$Post.2015.Percent.less.1mm, pair
 #   mean of the differences 
 # 13.405 
 # Conclusion: At 4 sites, Wolman pebble counts <1mm in 2015  were greater than Wolman pebble counts <1mm in 1994.
-
+# Values are getting signifcantly larger (worse) over time.  
 
 #1994 and 2015 Wolman percent sand and fines values
 t.test(Wolman$Pre.1994.percent.SAFN, Wolman$Post.2015.Percent.SAFN, paired=TRUE, alternative="greater")
@@ -229,6 +97,7 @@ t.test(Wolman$Pre.1994.percent.SAFN, Wolman$Post.2015.Percent.SAFN, paired=TRUE,
 #   mean of the differences 
 # 9.155 
 # Conclusion: At 4 sites, Wolman pebble counts percent SAFN in 2015  were greater than Wolman pebble counts percent SAFN in 1994.
+# Values are getting signifcantly larger (worse) over time.  
 
 #1994 and 2015 Wolman percent gravel values
 t.test(Wolman$Pre.1994.percent.gravel...fines, Wolman$Post.2015Percent.gravel...fines, paired=TRUE, alternative="greater")
@@ -243,6 +112,7 @@ t.test(Wolman$Pre.1994.percent.gravel...fines, Wolman$Post.2015Percent.gravel...
 #   mean of the differences 
 # 6.1525 
 # Conclusion: At 4 sites, Wolman pebble counts percent gravel in 2015  were greater than Wolman pebble counts percent gravel in 1994.
+# Values are getting signifcantly larger (worse) over time.  
 
 #2000 and 2015 Wolman D50 values
 t.test(Wolman$Pre.2000.D50..mm, Wolman$Post.2015.D50..mm, paired=TRUE, alternative="greater")
@@ -256,6 +126,191 @@ t.test(Wolman$Pre.2000.PercentSAFN, Wolman$Post.2015.Percent.SAFN, paired=TRUE, 
 #2000 and 2015 Wolman percent gravel values
 t.test(Wolman$Pre.2000.Percent.Gravel...fines, Wolman$Post.2015Percent.gravel...fines, paired=TRUE, alternative="greater")
 
+
+#################################
+#Are trends getting worse post BMP implmentation?
+
+#2005/06 and 2015 RBS LRBS values
+t.test(RBS$Pre.LRBS, RBS$Post.LRBS, paired=TRUE, alternative="greater")
+# Paired t-test
+# 
+# data:  RBS$Pre.LRBS and RBS$Post.LRBS
+# t = 1.1801, df = 11, p-value = 0.1314
+# alternative hypothesis: true difference in means is greater than 0
+# 95 percent confidence interval:
+#   -0.08235682         Inf
+# sample estimates:
+#   mean of the differences 
+# 0.1578175 
+# Conclusion: Post-implementation LRBS values could be greater than or equal to Pre-implementation LRBS values.
+# At least values are not getting smaller (further from zero). 
+
+#2005/06 and 2015 RBS percent sand and fines values
+t.test(RBS$Pre.Percent.Sand.and.Fines..PCT_SAFN., as.numeric(RBS$Post.Percent.Sand.and.Fines..PCT_SAFN.), paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  RBS$Pre.Percent.Sand.and.Fines..PCT_SAFN. and as.numeric(RBS$Post.Percent.Sand.and.Fines..PCT_SAFN.)
+# t = -0.17097, df = 11, p-value = 0.4337
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 4.558904
+# sample estimates:
+#   mean of the differences 
+# -0.4796667 
+# Conclusion: Post-implementation Percent SAFN values could be less than or equal to Pre-implementation Percent SAFN values.
+# At least values are not getting larger (further from zero). 
+
+#2005/06 and 2015 RBS percent sand values
+t.test(RBS$Pre.Percent.Sand..PCT_SA., RBS$Post.Percent.Sand..PCT_SA., paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  RBS$Pre.Percent.Sand..PCT_SA. and RBS$Post.Percent.Sand..PCT_SA.
+# t = -0.78705, df = 4, p-value = 0.2376
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 5.724318
+# sample estimates:
+#   mean of the differences 
+# -3.3502 
+# Conclusion: Post-implementation Percent Sand values could be less than or equal to Pre-implementation Percent Sand values.
+# At least values are not getting larger (further from zero). 
+
+#2005/06 and 2015 RBS percent fines values
+t.test(RBS$Pre.Percent.Fines..PCT_FN., RBS$Post.Percent.Fines..PCT_FN., paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  RBS$Pre.Percent.Fines..PCT_FN. and RBS$Post.Percent.Fines..PCT_FN.
+# t = 1.0441, df = 4, p-value = 0.8223
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 16.71406
+# sample estimates:
+#   mean of the differences 
+# 5.4948 
+# Conclusion: Post-implementation Percent Fines values could be less than or equal to Pre-implementation Percent Fines values.
+# At least values are not getting larger (further from zero). 
+
+#1994 and 2015 Wolman D50 values
+t.test(Wolman$Pre.1994.D50..mm, Wolman$Post.2015.D50..mm, paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  Wolman$Pre.1994.D50..mm and Wolman$Post.2015.D50..mm
+# t = -2.5404, df = 3, p-value = 0.04232
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf -0.2730085
+# sample estimates:
+#   mean of the differences 
+# -3.7075 
+# Conclusion: At 4 sites, D50 from Wolman pebble counts in 2015 D50 were less than D50 in 1994.
+# !! The values are getting larger (further from zero). 
+
+#1994 and 2015 Wolman percent less than 1 mm values
+t.test(Wolman$Pre.1994.percent.less.1mm, Wolman$Post.2015.Percent.less.1mm, paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  Wolman$Pre.1994.percent.less.1mm and Wolman$Post.2015.Percent.less.1mm
+# t = 3.5614, df = 3, p-value = 0.9811
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 22.26291
+# sample estimates:
+#   mean of the differences 
+# 13.405 
+# Conclusion: 2015 Wolman percent less than 1 mm values could be less than or equal to 1994 percent less than 1 mm values.
+# At least values are not getting larger (further from zero). 
+
+#1994 and 2015 Wolman percent sand and fines values
+t.test(Wolman$Pre.1994.percent.SAFN, Wolman$Post.2015.Percent.SAFN, paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  Wolman$Pre.1994.percent.SAFN and Wolman$Post.2015.Percent.SAFN
+# t = 2.7951, df = 3, p-value = 0.9659
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 16.86315
+# sample estimates:
+#   mean of the differences 
+# 9.155 
+# Conclusion: 2015 Wolman percent sand and fines values could be less than or equal to 1994 percent sand and fines values.
+# At least values are not getting larger (further from zero). 
+
+#1994 and 2015 Wolman percent gravel values
+t.test(Wolman$Pre.1994.percent.gravel...fines, Wolman$Post.2015Percent.gravel...fines, paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  Wolman$Pre.1994.percent.gravel...fines and Wolman$Post.2015Percent.gravel...fines
+# t = 2.4381, df = 3, p-value = 0.9537
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 12.09106
+# sample estimates:
+#   mean of the differences 
+# 6.1525 
+# Conclusion: 2015 Wolman percent gravel values could be less than or equal to 1994 percent gravel values.
+# At least values are not getting larger (further from zero). 
+
+#2000 and 2015 Wolman D50 values
+t.test(Wolman$Pre.2000.D50..mm, Wolman$Post.2015.D50..mm, paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  Wolman$Pre.2000.D50..mm and Wolman$Post.2015.D50..mm
+# t = -0.53835, df = 10, p-value = 0.3011
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 5.383153
+# sample estimates:
+#   mean of the differences 
+# -2.274545 
+# Conclusion: 2015 Wolman D50 values could be less than or equal to 2000 Wolman D50 values.
+# At least values are not getting larger (further from zero). 
+
+#2000 and 2015 Wolman percent less than 1 mm values
+t.test(Wolman$Pre.2000.Percent.Less.1mm, Wolman$Post.2015.Percent.less.1mm, paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  Wolman$Pre.2000.Percent.Less.1mm and Wolman$Post.2015.Percent.less.1mm
+# t = 0.088766, df = 10, p-value = 0.5345
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 7.59378
+# sample estimates:
+#   mean of the differences 
+# 0.3545455 
+# Conclusion: 2015 Wolman percent less than 1 mm values could be less than or equal to 2000 values.
+# At least values are not getting larger (further from zero). 
+
+#2000 and 2015 Wolman percent sand and fines values
+t.test(Wolman$Pre.2000.PercentSAFN, Wolman$Post.2015.Percent.SAFN, paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  Wolman$Pre.2000.PercentSAFN and Wolman$Post.2015.Percent.SAFN
+# t = -0.80785, df = 10, p-value = 0.219
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 3.38588
+# sample estimates:
+#   mean of the differences 
+# -2.722727 
+# Conclusion: 2015 Wolman percent sand and fines values could be less than or equal to 2000 values.
+# At least values are not getting larger (further from zero). 
+
+#2000 and 2015 Wolman percent gravel values
+t.test(Wolman$Pre.2000.Percent.Gravel...fines, Wolman$Post.2015Percent.gravel...fines, paired=TRUE, alternative="less")
+# Paired t-test
+# 
+# data:  Wolman$Pre.2000.Percent.Gravel...fines and Wolman$Post.2015Percent.gravel...fines
+# t = -0.56704, df = 10, p-value = 0.2916
+# alternative hypothesis: true difference in means is less than 0
+# 95 percent confidence interval:
+#   -Inf 4.594405
+# sample estimates:
+#   mean of the differences 
+# -2.091818 
+# Conclusion: 2015 Wolman percent gravel values could be less than or equal to 2000 values.
+# At least values are not getting larger (further from zero). 
+
+
 ##################################################################################
 #Delete the RBS notes with Google slope values 
 
@@ -263,4 +318,9 @@ RBS.slope.adj <- RBS[RBS$Post.Notes == "",]
 
 #2005/06 and 2015 RBS LRBS values
 t.test(RBS.slope.adj$Pre.LRBS, RBS.slope.adj$Post.LRBS, paired=TRUE, alternative="less")
+# Conclusion: Post-implementation LRBS values could be less than or equal to Pre-implementation LRBS values.
 t.test(RBS.slope.adj$Pre.LRBS, RBS.slope.adj$Post.LRBS, paired=TRUE, alternative="greater")
+# Conclusion: Post-implementation LRBS values could be greater than or equal to Pre-implementation LRBS values.
+#Not getting better or worse
+
+##################################################################################
